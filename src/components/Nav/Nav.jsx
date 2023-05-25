7542658225e8ef6ea76a07eb6235c435989383db
SANDRA HERNANDEZ VALERO <sandy_hv5@hotmail.com>
2023-05-25 15:30:26 -0600

    first commit

---
+++ src/components/Nav/Nav.jsx
@@ -0,0 +1,10 @@
+import logo from '../../assets/react.svg'
+import './Nav.css'
+
+export default function Nav () {
+    return (
+        <nav className='nav'>
+            <img src={logo} alt="logo" />
+        </nav>
+    )
+}
